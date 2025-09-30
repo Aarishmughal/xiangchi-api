be261d0970cbf982ab7ee8fbd72246febd449592
Muhammad Aarish <223570@students.au.edu.pk>
2025-09-30 19:05:58 +0500

    Fixed User Routes Tag

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(cookieParser());
 
 // Routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/user', userRouter);
 app.use('/api/v1/games', require('./routes/gameRoutes'));
 
 // Error Handling Middleware
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ router.post(
   authController.protect,
   authController.refreshToken
 );
-router.get('/logout', authController.logout);
+router.get('/logout', authController.protect, authController.logout);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
